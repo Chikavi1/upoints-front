490963ae970997dc6cb09c89157407f28b463086
l.rojas <luisrojas@radi.pet>
2024-12-02 16:58:20 -0600

    landings

--- src/app/landing/landing-client/landing-client.component.ts
+++ src/app/landing/landing-client/landing-client.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-client',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './landing-client.component.html',
   styleUrl: './landing-client.component.scss'
 })
 export class LandingClientComponent {
+
+  main_features = 1;
+
+  changeFeatures(feature: number) {
+    console.log( feature,'f:');
+    this.main_features = feature;
+  }
 
 }
