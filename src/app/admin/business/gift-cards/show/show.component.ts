a888ecc109e68a6e5441ca37d65d9e7502f255a8
l.rojas <luisrojas@radi.pet>
2024-12-27 12:34:09 -0600

    last changes

--- src/app/admin/business/gift-cards/show/show.component.ts
+++ src/app/admin/business/gift-cards/show/show.component.ts
@@ -12,7 +12,23 @@ export class ShowComponent {
   constructor(private router: Router) {}
 
   checkout() {
+
+    const cartInfo = {
+      "amount": 100,
+      "from": "Luis Rojas",
+      "to": "chikavi10@gmail.com",
+      "message": "Mensaje de prueba",
+    }
+
+    localStorage.setItem('cartInfo', JSON.stringify(cartInfo));
     this.router.navigate(['/checkout']);
   }
+
+   allowOnlyNumbers(event: KeyboardEvent): void {
+    const isNumber = /^[0-9]$/.test(event.key);  
+    if (!isNumber) {
+      event.preventDefault();  
+    }
+}
 
 }
--- src/app/public/checkout/checkout.component.ts
+++ src/app/public/checkout/checkout.component.ts
@@ -1,19 +1,47 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CrudService } from '../../services/crud.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [HttpClientModule],
+  providers: [CrudService],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
 export class CheckoutComponent {
 
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private _api:CrudService) { }
 
 
   pay() {
-    this.router.navigate(['/success']);
+
+    const data: any = {
+      "amount": 100,
+      "currency": "ARS",
+      "description": "Tarjeta de regalo de Carls jr",
+      "source": {
+        "object": "card",
+        "number": "4242 4242 4242 4242",
+        "exp_month": 8,
+        "exp_year": 2025,
+        "cvc": "314"
+      },
+      "from": "Luis Rojas",
+      "to": "chikavi10@gmail.com",
+      "message": "Mensaje de prueba",
+    }
+
+
+    this._api.post('gift-card/pay', data).subscribe((response: any) => {
+      
+      console.log('respuesta: ', response);
+
+    });
+
+
+    // this.router.navigate(['/success']);
   }
 }
